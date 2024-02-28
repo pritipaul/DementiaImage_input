885a4c582d5ec9e4acba53997499d0245ebf703c
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 17:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-# import streamlit as st
-
-# picture = st.camera_input("Take a picture")
-
-# if picture:
-#     st.image(picture)
-
 import streamlit as st
-import tensorflow as tf
 
-img_file_buffer = st.camera_input("Take a picture")
+picture = st.camera_input("Take a picture")
 
-if img_file_buffer is not None:
-    # To read image file buffer as a 3D uint8 tensor with TensorFlow:
-    bytes_data = img_file_buffer.getvalue()
-    img_tensor = tf.io.decode_image(bytes_data, channels=3)
+if picture:
+    st.image(picture)
 
-    # Check the type of img_tensor:
-    # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
-    st.write(type(img_tensor))
-
-    # Check the shape of img_tensor:
-    # Should output shape: (height, width, channels)
-    st.write(img_tensor.shape)
