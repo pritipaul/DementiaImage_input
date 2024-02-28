61c1c50ad3c030703c444b3eb438b8f3032dc87a
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 19:11:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import os
 
 # Take a picture
 picture = st.camera_input("Take a picture")
@@ -16,11 +17,19 @@ if picture:
             # Convert UploadedFile object to PIL Image
             image = Image.open(picture)
 
+        #     # Save the image
+        #     file_path = "image/{image_name}.jpg"  # Change the path as needed
+        #     image.save(file_path)
+        #     st.success(f"Image saved as '{image_name}.jpg' successfully!")
+        # else:
+        #     st.warning("Please enter an image name!")
+        if not os.path.exists("images"):
+                os.makedirs("images")
+
             # Save the image
-            file_path = "image/{image_name}.jpg"  # Change the path as needed
+            file_path = os.path.join("images", f"{image_name}.jpg")
             image.save(file_path)
             st.success(f"Image saved as '{image_name}.jpg' successfully!")
         else:
             st.warning("Please enter an image name!")
-
 
