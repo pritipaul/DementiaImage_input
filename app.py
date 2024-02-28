2a9fc8c3cfc1d1fd6df45c6b4c645b0f3f1e6c2b
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 17:46:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if picture:
             image = Image.open(picture)
 
             # Save the image
-            file_path = f"path/to/your/image/{image_name}.jpg"  # Change the path as needed
+            file_path = f"/image/{image_name}.jpg"  # Change the path as needed
             image.save(file_path)
             st.success(f"Image saved as '{image_name}.jpg' successfully!")
         else:
