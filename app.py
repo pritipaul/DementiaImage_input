95dc1565010c81e7e73c905625271ff58ea79fb5
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 19:49:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,17 @@ if picture:
 
             # Save the image
             file_path = os.path.join("images", f"{image_name}.jpg")
-            image.save(file_path)
-            st.success(f"Image saved as '{image_name}.jpg' successfully!")
-        else:
-            st.warning("Please enter an image name!")
-
+        #     image.save(file_path)
+        #     st.success(f"Image saved as '{image_name}.jpg' successfully!")
+        # else:
+        #     st.warning("Please enter an image name!")
+            try:
+                image.save(file_path)
+                # Displaying a success message if the image is successfully saved
+                if os.path.exists(file_path):  # Check if the file exists
+                    st.success(f"Image saved as '{image_name}.jpg' successfully!")
+                else:
+                    st.error("Failed to save image!")
+            except Exception as e:
+                st.error(f"Error occurred while saving image: {str(e)}")
 
