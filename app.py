a4b7228d038c99441182ca9abe30c7cf34322fe4
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 19:13:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
+# import streamlit as st
+# from PIL import Image
+# import os
+
+# # Take a picture
+# picture = st.camera_input("Take a picture")
+# if picture:
+#     # Display the image
+#     st.image(picture)
+
+#     # Input field for image name
+#     image_name = st.text_input('Enter image name (without extension):')
+
+#     # Button to save the image
+#     if st.button('Save Image'):
+#         if image_name:
+#             # Convert UploadedFile object to PIL Image
+#             image = Image.open(picture)
+
+        #     # Save the image
+        #     file_path = "image/{image_name}.jpg"  # Change the path as needed
+        #     image.save(file_path)
+        #     st.success(f"Image saved as '{image_name}.jpg' successfully!")
+        # else:
+        #     st.warning("Please enter an image name!")
+
+
+
+
 import streamlit as st
 from PIL import Image
 import os
@@ -16,14 +45,9 @@ if picture:
         if image_name:
             # Convert UploadedFile object to PIL Image
             image = Image.open(picture)
-
-        #     # Save the image
-        #     file_path = "image/{image_name}.jpg"  # Change the path as needed
-        #     image.save(file_path)
-        #     st.success(f"Image saved as '{image_name}.jpg' successfully!")
-        # else:
-        #     st.warning("Please enter an image name!")
-        if not os.path.exists("images"):
+            
+            # Create directory if it doesn't exist
+            if not os.path.exists("images"):
                 os.makedirs("images")
 
             # Save the image
@@ -32,4 +56,3 @@ if picture:
             st.success(f"Image saved as '{image_name}.jpg' successfully!")
         else:
             st.warning("Please enter an image name!")
-
