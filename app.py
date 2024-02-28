5bd6e18d48d04b0869de5bad1f2e9441196f99f5
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 18:14:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,27 @@
-# import streamlit as st
-
-# picture = st.camera_input("Take a picture")
-
-# if picture:
-#     st.image(picture)
-# import streamlit as st
-
-# # Display the image
-# if picture:
-#     st.image(picture)
-
-# # Input field for image name
-# image_name = st.text_input('Enter image name (without extension):')
-
-# # Button to save the image
-# if st.button('Save Image'):
-#     if picture:
-#         if image_name:
-#             file_path = f"path/to/your/image/{image_name}.jpg"  # Change the path as needed
-
-#             picture.save(file_path)
-#             st.success(f"Image saved as '{image_name}.jpg' successfully!")
-#         else:
-#             st.warning("Please enter an image name!")
-#     else:
-#         st.warning("No image to save!")
-
-
 import streamlit as st
 from PIL import Image
 
 # Take a picture
 picture = st.camera_input("Take a picture")
-if 'myimage' not in session_state.key():
-    session_state['myimage'] = None
 if picture:
     # Display the image
-    session_state['myimage'] = st.image(picture)
+    st.image(picture)
 
-#     # Input field for image name
-#     image_name = st.text_input('Enter image name (without extension):')
+    # Input field for image name
+    image_name = st.text_input('Enter image name (without extension):')
 
-#     # Button to save the image
-#     if st.button('Save Image'):
-#         if image_name:
-#             # Convert UploadedFile object to PIL Image
-#             image = Image.open(picture)
+    # Button to save the image
+    if st.button('Save Image'):
+        if image_name:
+            # Convert UploadedFile object to PIL Image
+            image = Image.open(picture)
 
-#             # Save the image
-#             file_path = f"/image/{image_name}.jpg"  # Change the path as needed
-#             image.save(file_path)
-#             st.success(f"Image saved as '{image_name}.jpg' successfully!")
-#         else:
-#             st.warning("Please enter an image name!")
+            # Save the image
+            file_path = f"/image/{image_name}.jpg"  # Change the path as needed
+            image.save(file_path)
+            st.success(f"Image saved as '{image_name}.jpg' successfully!")
+        else:
+            st.warning("Please enter an image name!")
 
 
 
