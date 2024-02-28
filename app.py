fcf21ddb52cef477fde44f37de8bfea424f2260f
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 18:00:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,29 +28,35 @@
 
 
 import streamlit as st
-from PIL import Image
+# from PIL import Image
 
-# Take a picture
-picture = st.camera_input("Take a picture")
+# # Take a picture
+# picture = st.camera_input("Take a picture")
 
-if picture:
-    # Display the image
-    st.image(picture)
+# if picture:
+#     # Display the image
+#     st.image(picture)
 
-    # Input field for image name
-    image_name = st.text_input('Enter image name (without extension):')
+#     # Input field for image name
+#     image_name = st.text_input('Enter image name (without extension):')
 
-    # Button to save the image
-    if st.button('Save Image'):
-        if image_name:
-            # Convert UploadedFile object to PIL Image
-            image = Image.open(picture)
+#     # Button to save the image
+#     if st.button('Save Image'):
+#         if image_name:
+#             # Convert UploadedFile object to PIL Image
+#             image = Image.open(picture)
 
-            # Save the image
-            file_path = f"/image/{image_name}.jpg"  # Change the path as needed
-            image.save(file_path)
-            st.success(f"Image saved as '{image_name}.jpg' successfully!")
-        else:
-            st.warning("Please enter an image name!")
+#             # Save the image
+#             file_path = f"/image/{image_name}.jpg"  # Change the path as needed
+#             image.save(file_path)
+#             st.success(f"Image saved as '{image_name}.jpg' successfully!")
+#         else:
+#             st.warning("Please enter an image name!")
 
 
+
+
+
+def main(sub):
+    st.subheader(sub)
+    st.camera_input("Take a picture")
